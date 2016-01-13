d85cfdbc3ac8a51bb86494bb79b49cfb04563655
Alex Kane <wkanew@yahoo.co.nz>
2016-01-13 20:09:20 +0100

    no message

--- src/image.rs
+++ src/image.rs
@@ -1,10 +1,19 @@
 
-mod image {
-    use palette::Color;
 
-    struct Image {
-        width: u32,
-        height: u32,
-        pixels: Vec<Color>,
-    }
+use palette::Color;
+
+pub struct Image {
+    width: u32,
+    height: u32,
+    pixels: Vec<Color>,
+}
+
+impl Image {
+	pub fn new(width: u32, height: u32) -> Image {
+		Image {
+			width: width,
+			height: height,
+			pixels: vec![],
+		}
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 extern crate palette;
+use image::Image;
 pub mod image;
 
 fn main() {
     println!("Hello, world!");
-                let im1 : image::Image;
+    let im1 = Image::new(2,3);
 }
