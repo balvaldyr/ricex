2be165ba49ba0282a3d9804331a2d5a77f389844
Alex Kane <wkanew@yahoo.co.nz>
2016-01-13 17:48:42 +0100

    Initial commit. does not compile

---
+++ src/image.rs
@@ -0,0 +1,10 @@
+
+mod image {
+    use palette::Color;
+
+    struct Image {
+        width: u32,
+        height: u32,
+        pixels: Vec<Color>,
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+extern crate palette;
+pub mod image;
+
+fn main() {
+    println!("Hello, world!");
+                let im1 : image::Image;
+}
